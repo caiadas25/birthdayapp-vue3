cb78c4347547019f4e5df777ebeef6a3f8944aa4
Nuno Caiadas <nunocaiadas25@gmail.com>
2022-02-11 01:10:11 +0000

    improved register view

--- src/supabase/init.ts
+++ src/supabase/init.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
+//@ts-ignore
 const supabaseUrl = import.meta.env.VUE_APP_SUPABASE_URL
+//@ts-ignore
 const supabaseAnonKey = import.meta.env.VUE_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,62 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Register view!"/>
+  <div>{{ email }}</div>
+  <div v-if="errorMessage">
+    <p>{{ errorMessage }}</p>
   </div>
+
+  <form>
+    <h1>Register</h1>
+    <div>
+      <label for="email">Email</label>
+      <input type="text" required id="email" v-model="email" />
+    </div>
+    <div>
+      <label for="email">Password</label>
+      <input type="password" required id="password" v-model="password" />
+    </div>
+    <div>
+      <label for="email">Confirm Password</label>
+      <input
+        type="password"
+        required
+        id="confirmPassword"
+        v-model="confirmPassword"
+      />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { Options, Vue } from "vue-class-component";
+import { supabase } from "@/supabase/init";
+import { ref } from "vue";
 @Options({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
-export default class Register extends Vue {}
+export default class Register extends Vue {
+  email: HTMLInputElement | null = null;
+  password: HTMLInputElement | null = null;
+  confirmPassword: HTMLInputElement | null = null;
+  errorMessage: HTMLInputElement | null = null;
+
+  async register() {
+    if (this.password?.value === this.confirmPassword?.value) {
+      try {
+        const { error } = await supabase.auth.signUp({
+          email: this.email!.value,
+          password: this.password!.value,
+        });
+        if (error) throw error;
+        this.$router.push("/login");
+      } catch (error: any) {
+        this.errorMessage!.value = error.message;
+      }
+    }
+    this.errorMessage!.value = "Error: Passwords do not match.";
+    setTimeout(() => {
+      this.errorMessage!.value = "";
+    }, 5000);
+  }
+}
 </script>
